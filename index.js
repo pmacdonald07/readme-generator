ec469967d5ffa8a731e025fa8aeddd1831f83d90
Patrick M <patrickmacdonald07@gmail.com>
2022-09-25 16:04:25 -0400

    added questions and license switch statements

--- index.js
+++ index.js
@@ -1,7 +1,110 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter the title of your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "How would you describe your project? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a description of your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "instillation",
+    message: "What are the installation requirements for your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How will people use your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license is your project covered under? (Required)",
+    choices: [
+      "Apache License",
+      "GNU General Public License v3.0",
+      "MIT License",
+      "None",
+    ],
+    validate: (licenseInput) => {
+      if (licenseInput) {
+        return true;
+      } else {
+        console.log("Please enter a license associated with your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "What are the contribution guidelines for your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "What tests have you created for your project, and what are the instructions for using them?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub username!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message:
+      "What email address should users send their questions to? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a description of your project!");
+        return false;
+      }
+    },
+  },
+];
+
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
+
+promptUser().then((data) => {
+  console.log(data);
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,5 +114,3 @@ function init() {}
 
 // Function call to initialize app
 //init();
-
-console.log("Hello");
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache License":
+      return `[Apache Badge](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+    case "GNU General Public License v3.0":
+      return `![GNU Badge](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+    case "MIT License":
+      return `[MIT Badge](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache License":
+      return `[Apache License Link](https://opensource.org/licenses/Apache-2.0)`;
+    case "GNU General Public License v3.0":
+      return `[GNU License Link](https://www.gnu.org/licenses/gpl-3.0)`;
+    case "MIT License":
+      return `[MIT License Link](https://opensource.org/licenses/MIT)`;
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
